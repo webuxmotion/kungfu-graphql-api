d5e5c14dc7c1b3452538c37f9297a956ea19d80b
Andrii Pereverziev <andriipereverziev@Andriis-MacBook-Pro.local>
2020-10-24 19:05:39 +0300

    Add playground

--- index.js
+++ index.js
@@ -58,7 +58,12 @@ const resolvers = {
   }
 };
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({ 
+  typeDefs,
+  resolvers,
+  introspection: true,
+  playground: true
+});
 
 server.listen({
   port: process.env.PORT || 4000
